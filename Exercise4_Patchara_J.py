6670f038fb9c330eea330d650c5aa5ddcf0a528d
PatcharaPetch <109201582+PatcharaPetch@users.noreply.github.com>
2022-07-15 20:03:44 +0700

    Add files via upload

---
+++ Exercise4_Patchara_J.py
@@ -0,0 +1,11 @@
+Eng = 50
+Gen = 62.5
+intro = 85
+compro = 90
+print("-----------------------------------------------")
+print("-------Your Score-------")
+print("Foundation English :",Eng)
+print("General Business :",Gen )
+print("Introduction to Computer Systems :",intro)
+print("Computer Programming :",compro)
+print("-----------------------------------------------")
